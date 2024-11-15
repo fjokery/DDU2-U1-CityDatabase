c801d14c3fc92be2530f2c9d93c1484d895920e5
Felicia <151528329+fjokery@users.noreply.github.com>
2024-11-15 14:02:56 +0100

    Added function userInputObject
    added a function to see if the city exsits in the database, also returns the whole cityobjects which is used in getCityDistance to loop thru all ids in distances

--- website/index.js
+++ website/index.js
@@ -1,17 +1,37 @@
 // Recommended: All functions declared here
-function createBox(cityName) { //the problem with this one is maybe idk how to target and stuff?
+function createBox(cityName) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("cityBox");
     newDiv.textContent = cityName; //if text content = same cities.name ... target or furthest
     document.getElementById("cities").append(newDiv);
 }
 
-function getCityDistance(userCityInput){
+function userInputObject(userInput) {
+    cityFound = false;
+    let cityObject = null;
+    for (let city in cities) {
+        if (userInput == cities[city].name) {
+            cityFound = true;
+            cityObject = cities[city]; // Save the city object
+            break;
+        }
+    }
+    if (cityFound = false) {
+        //input function here 
+    }
+    return cityObject
+}
+function getCityDistance(userInputObject) {
     let shortestDistance;
     let nearestCity;
     let longestDistance;
     let farthestCity;
-
+    for (let distance of distances) {
+        if (distance.city1 == userInputObject.id || distance.city2 == userInputObject.id) {
+            //hur kollar jag id???
+            console.log("it exsists");
+        }
+    }
 }
 // Recommended: constants with references to existing HTML-elements
 
@@ -23,4 +43,8 @@ for (let i = 0; i < cities.length; i++) {
 }
 
 const userInput = prompt("Vilken stad?") // USER INPUT
+
+console.log(userInputObject(userInput));
+getCityDistance(userInputObject(userInput));
+
 
