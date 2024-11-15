256765d591fa025723ceea03e878ad54aa72cc1b
Felicia <151528329+fjokery@users.noreply.github.com>
2024-11-15 13:22:50 +0100

    added function createBox()
    creates (div) boxes for the html with the class "cityBox"

--- website/index.js
+++ website/index.js
@@ -1,6 +1,18 @@
 // Recommended: All functions declared here
-
+function createBox(cityName) { //the problem with this one is maybe idk how to target and stuff?
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("cityBox");
+    newDiv.textContent = cityName; //if text content = same cities.name ... target or furthest
+    document.getElementById("cities").append(newDiv);
+}
 // Recommended: constants with references to existing HTML-elements
 
 // Recommended: Ask for the city name and then the rest of the code
+
+//Skapar alla boxes med en loop
+for (let i = 0; i < cities.length; i++) {
+    createBox(cities[i].name)
+}
+
+const userInput = prompt("Vilken stad?") // USER INPUT
 
